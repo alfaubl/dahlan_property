3a4bbfbfd564025b2951d497628ee13fceb35a57
alfaubl <80946135+alfaubl@users.noreply.github.com>
2026-02-19 18:52:00 -0800

    l

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', 'Dahlan Property - Marketplace Properti Terbaik')</title>
 
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -12,38 +16,34 @@
     <!-- AOS Animation -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
-   
+    <!-- Custom CSS -->
     @include('layouts.app-css')
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
-
-    <!-- Page Title -->
-    <title>@yield('title', 'Dahlan Property - Marketplace Properti Terbaik')</title>
-
-    <!-- Page Specific Styles -->
     @yield('styles')
+
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
 </head>
 
 <body>
-    <!-- Navbar -->
+
     @include('layouts.navbar')
 
-    <!-- Main Content -->
     <main>
         @yield('content')
     </main>
 
-    <!-- Footer -->
     @include('layouts.footer')
 
-    <!-- Scripts -->
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- AOS JS -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
     <!-- Custom JS -->
-    @include('layouts.app-js') ← PAKAI DASH, BUKAN TITIK!
+    @include('layouts.app-js')
 
-    <!-- Page Specific Scripts -->
     @yield('scripts')
+
 </body>
+</html>
--- storage/framework/views/7cf8c8490090523f1f0efd5a75a17e2af912c946.php
+++ storage/framework/views/7cf8c8490090523f1f0efd5a75a17e2af912c946.php
@@ -1,7 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title><?php echo $__env->yieldContent('title', 'Dahlan Property - Marketplace Properti Terbaik'); ?></title>
 
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -12,38 +16,35 @@
     <!-- AOS Animation -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
-   
+    <!-- Custom CSS -->
     <?php echo $__env->make('layouts.app-css', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="<?php echo e(asset('images/favicon.ico')); ?>">
-
-    <!-- Page Title -->
-    <title><?php echo $__env->yieldContent('title', 'Dahlan Property - Marketplace Properti Terbaik'); ?></title>
-
-    <!-- Page Specific Styles -->
     <?php echo $__env->yieldContent('styles'); ?>
+
+    <link rel="icon" type="image/x-icon" href="<?php echo e(asset('images/favicon.ico')); ?>">
 </head>
 
 <body>
-    <!-- Navbar -->
+
     <?php echo $__env->make('layouts.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
-    <!-- Main Content -->
     <main>
         <?php echo $__env->yieldContent('content'); ?>
     </main>
 
-    <!-- Footer -->
     <?php echo $__env->make('layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
-    <!-- Scripts -->
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- AOS JS -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
     <!-- Custom JS -->
-    <?php echo $__env->make('layouts.app-js', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?> ← PAKAI DASH, BUKAN TITIK!
+    <?php echo $__env->make('layouts.app-js', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
-    <!-- Page Specific Scripts -->
     <?php echo $__env->yieldContent('scripts'); ?>
-</body><?php /**PATH C:\xampp\htdocs\dahlan_project\resources\views/layouts/app.blade.php ENDPATH**/ ?>
+
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\dahlan_project\resources\views/layouts/app.blade.php ENDPATH**/ ?>
